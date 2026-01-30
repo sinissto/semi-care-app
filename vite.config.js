472597f84eaa360961fecd27f413ee83d717f132
Sinisa <sinisha.stojanovic@gmail.com>
2026-01-30 10:25:56 +0100

    React Router install

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
+import { reactRouter } from "@react-router/dev/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwind()],
+  plugins: [react(), tailwind(), reactRouter()],
 });
