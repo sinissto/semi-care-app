d37456734139b13cafeaf90089f42b49d7f031b2
Sinisa <sinisha.stojanovic@gmail.com>
2026-02-01 19:13:00 +0100

    Footer - layout, and posible info

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,15 +1,42 @@
+import { Link } from "react-router";
+import logo from "@/assets/images/LogoLarge.png";
+
 const Footer = () => {
   return (
-    <footer className="bg-primary-tint p-6">
-      <div className="max-w-8xl mx-auto grid md:grid-cols-2 gap-6">
-        <div>
-          <p className="font-semibold">Semi Care</p>
-          <p className="text-sm">Professional outpatient care</p>
-        </div>
+    <footer className="bg-primary1 rounded-t-4xl p-6">
+      <div className="max-w-8xl mx-auto flex flex-col items-center justify-center gap-5 px-10 pt-16 mb-8">
+        <h2 className="text-6xl text-white font-bold ">Semi Care</h2>
+        <p className="text-2xl text-center font-semibold text-white uppercase">
+          How can we make your life happier?
+        </p>
+        <p className="text-xl text-center font-semibold text-white uppercase">
+          We offer a free consultation!
+        </p>
+        <Link
+          to={"/contact"}
+          className={
+            "text-xl text-center text-primary1 font-semibold px-6 py-3 bg-white rounded-[5px]"
+          }
+        >
+          Let's find out
+        </Link>
+      </div>
+
+      <div
+        className={
+          "max-w-8xl mx-auto flex flex-col md:flex-row items-center justify-between gap-8 bg-white p-8 rounded-2xl"
+        }
+      >
+        <img src={logo} className={"w-[30%] h-auto rounded-2xl"} />
+
+        <div>SERVICES</div>
+
         <div className="text-sm">
           <p>Email: info@semi-care.de</p>
           <p>Phone: +01 23 45 67 890</p>
         </div>
+
+        <div>SOCIALS, MAYBE</div>
       </div>
     </footer>
   );
