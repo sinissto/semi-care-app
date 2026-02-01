ca664f5dbd663283c1ddf66f068cf9ca17f0df37
Sinisa <sinisha.stojanovic@gmail.com>
2026-02-01 11:03:24 +0100

    SpecialUs section - Swiper install

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
-import CareTypes from "../components/home/CareTypes";
 import Hero from "../components/home/Hero";
+import CareTypes from "../components/home/CareTypes";
+import SpecialUs from "@/components/home/SpecialUs";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <CareTypes />
+      <SpecialUs />
     </>
   );
 };
