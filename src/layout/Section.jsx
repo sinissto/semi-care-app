22c391acf77c1a9501d2b3e9d5d046d782a35ea6
Sinisa <sinisha.stojanovic@gmail.com>
2026-02-06 17:37:08 +0100

    Section layout - rounded bottom border (last-of-type)

--- src/layout/Section.jsx
+++ src/layout/Section.jsx
@@ -2,7 +2,7 @@ const Section = ({ children, className }) => {
   return (
     <section>
       <div
-        className={`py-12 max-w-8xl mx-auto px-8 ${
+        className={`py-12 max-w-8xl mx-auto px-8 last-of-type:rounded-b-2xl ${
           className ? className : "bg-primary-tint"
         }`}
       >
